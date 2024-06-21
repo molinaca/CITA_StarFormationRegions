8b3dfc29afe1743def7e7174e23c836eb9cb5d5c
Ana Molina Colina <molinaca@mcmaster.ca>
2024-06-21 14:35:12 -0400

    Added settings_template so that user can change settings, added settings.py to git ignore so that it doesnt overwrite user settings

---
+++ settings_template.py
@@ -0,0 +1,10 @@
+#File that contains settings from where to obtain data and to store it
+
+# Change this file's content based on your needs
+# Rename changed file as "settings.py"
+
+csv_files_path = '/fs/lustre/cita/molinaca/CSV_Files'
+
+data_files_path = '/fs/lustre/cita/molinaca/'
+
+image_path = '/fs/lustre/cita/molinaca/Images'
