68b52aa563adcdd20b4082be60568239f94a1ffc
Ana Molina Colina <molinaca@mcmaster.ca>
2024-06-21 12:24:54 -0400

    Added variables for data files and csv files

--- settings.py
+++ settings.py
@@ -1,3 +1,7 @@
 #File used to change path for where to store images
 
+csv_files_path = '/fs/lustre/cita/molinaca/CSV_Files'
+
+data_files_path = '/fs/lustre/cita/molinaca/'
+
 image_path = '/fs/lustre/cita/molinaca/Images'
